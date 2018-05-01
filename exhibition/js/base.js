f6ace64ae5c96fa925d63e503db56eccd2dd998b
懒洋洋\猪头 <963126366@qq.com>
2018-05-01 21:36:07 +0800

    恢复了误删文件

--- exhibition/js/base.js
+++ exhibition/js/base.js
@@ -1,9 +1,10 @@
 //运行模式,local 本地模式 本地服务器,remote 远程模式,远程服务器
-var getData = true;
+var dataOn = true;
+var isLogin = false;
 var mode = 'local';
-var url;
+var baseUrl;
 if(mode === 'local'){
-	url = 'http://localhost:8080/'
+	baseUrl = 'http://localhost:8080'
 }else{
-	url = '';
+	baseUrl = '';
 }
---
+++ exhibition/js/index.js
@@ -0,0 +1,146 @@
+//首页数据处理
+
+$(function(){
+	if(!dataOn){
+		return;
+	}
+	//获取幻灯片数据
+	$.ajax({
+		url:baseUrl+'/api/sliderImage/list',
+		method:'get',
+		dataType:'json',
+		success:function(response){
+			if(response.status == 200){
+				setSliderImage(response.data);
+			}else{
+				layer.alert('加载幻灯片数据失败', {icon: 2});
+			}
+		},
+		error:function(){
+			
+		}
+	});
+	//获取传奇书画数据
+	$.ajax({
+		url:baseUrl+'/api/image?type=1&offset=0&limit=6',
+		method:'get',
+		dataType:'json',
+		success:function(response){
+			if(response.status == 200){
+				setCqshData(response.data);
+			}else{
+				
+			}
+		},
+		error:function(){
+			
+		}
+	});
+	//获取当代书画数据
+	$.ajax({
+		url:baseUrl+'/api/image?type=2&offset=0&limit=3',
+		method:'get',
+		dataType:'json',
+		success:function(response){
+			if(response.status == 200){
+				setDdshData(response.data);
+			}else{
+				
+			}
+		},
+		error:function(){
+			
+		}
+	});
+	//获取展厅数据
+	$.ajax({
+		url:baseUrl+'/api/gallery?offset=0&limit=5',
+		method:'get',
+		dataType:'json',
+		success:function(response){
+			if(response.status == 200){
+				setGalleryData(response.data);
+			}else{
+				
+			}
+		},
+		error:function(){
+			
+		}
+	});
+	//获取展厅数据
+	$.ajax({
+		url:baseUrl+'/api/gallery?offset=0&limit=5',
+		method:'get',
+		dataType:'json',
+		success:function(response){
+			if(response.status == 200){
+				setGalleryData(response.data);
+			}else{
+				
+			}
+		},
+		error:function(){
+			
+		}
+	});
+	//获取竞拍数据
+	$.ajax({
+		url:baseUrl+'/api/auction?offset=0&limit=8',
+		method:'get',
+		dataType:'json',
+		success:function(response){
+			if(response.status == 200){
+				setAuctionData(response.data);
+			}else{
+				
+			}
+		},
+		error:function(){
+			
+		}
+	});
+});
+/**
+ * 置幻灯片数据
+ */
+function setSliderImage(images){
+	
+}
+/**
+ * 置传奇书画数据
+ */
+ 
+function setCqshData(datas){
+	 
+}
+ 
+/**
+ * 置当代书画数据
+ */
+ 
+function setDdshData(datas){
+	 
+}
+
+/**
+ * 置展厅数据
+ */
+ 
+function setGalleryData(datas){
+	 
+}
+/**
+ * 竞拍数据
+ */
+function setAuctionData(datas){
+	
+}
+
+/**
+ * 置新闻数据
+ */
+ 
+function setNewsData(datas){
+	 
+}
