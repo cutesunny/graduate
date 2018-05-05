cbbae340bfb8fa79c4626bd225a054b9c201afeb
lyrric <xx20510@163.com>
2018-05-05 21:04:53 +0800

    首页的当代书画改为由js引入

--- exhibition/js/index.js
+++ exhibition/js/index.js
@@ -3,6 +3,7 @@
 
 $(function(){
 	if(!dataOn){
+		//轮播图
         $('#slider_index').append('<li data-target="#carousel1" data-slide-to="0" class="active"></li>');
         $('#slider_index').append('<li data-target="#carousel1" data-slide-to="1"></li>');
         $('#slider_index').append('<li data-target="#carousel1" data-slide-to="2"></li>');
@@ -10,6 +11,25 @@ $(function(){
 		$('#slider_image').append('<div class="item active"><img src="img/photo1.jpg" alt="..."></div>');
         $('#slider_image').append('<div class="item"><img src="img/photo1.jpg" alt="..."></div>');
         $('#slider_image').append('<div class="item"><img src="img/photo1.jpg" alt="..."></div>');
+        //当代书画
+        $('#contemporary_painting').append('<div class="col-lg-4"> <figure class="effect-roxy">' +
+            ' 	<img src="img/part2-wudayu.jpg" alt="part2-wudayu.jpg" />' +
+            ' <figcaption><h2>吴大羽绘画 </h2>' +
+            ' <p>吴大羽，宜兴宜城镇人，我国杰出的油画家，艺术教育家，中国现代画事业的奠基人之一。</p>' +
+            ' <a href="ddsh-details1.html">View more</a> </figcaption></figure>' +
+            '</div>');
+        $('#contemporary_painting').append('<div class="col-lg-4"> <figure class="effect-roxy">' +
+            ' 	<img src="img/part2-sajinte.jpg" alt="part2-wudayu.jpg" />' +
+            ' <figcaption><h2>萨金特绘画</span></h2>' +
+            ' <p>约翰·辛格·萨金特John Singer Sargent(1856—1925)美国画家。生于意大利佛罗伦萨。 1874年在卡罗律斯—迪朗的巴黎画室学画，受印象派影响。</p>' +
+            ' <a href="ddsh-details1.html">View more</a> </figcaption></figure>' +
+            '</div>');
+        $('#contemporary_painting').append('<div class="col-lg-4"> <figure class="effect-roxy">' +
+            ' 	<img src="img/part2-songyonghong.jpg" alt="part2-wudayu.jpg" />' +
+            ' <figcaption><h2>宋永红绘画 </h2>' +
+            ' <p>吴宋永红1966年生于河北省曲阳县1988年毕业于浙江美术学院版画系（现为中国美术学院）。</p>' +
+            ' <a href="ddsh-details1.html">View more</a> </figcaption></figure>' +
+            '</div>');
 		return;
 	}
 	//获取幻灯片数据
